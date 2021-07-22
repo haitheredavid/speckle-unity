a6fe27ba31b78e8f15730ce95d7737ba0318b862
David Morgan <30870056+haitheredavid@users.noreply.github.com>
2021-07-22 08:34:16 -0500

    dev: updated dll's to project

---
+++ Assets/Speckle Connector/ConverterUnity.ViewTo.cs
@@ -0,0 +1,20 @@
+﻿using Objects.ViewTo;
+using ViewTo;
+using ViewTo.Objects;
+
+namespace Objects.Converter.Unity
+{
+  public partial class ConverterUnity
+  {
+
+    public void Item()
+    {
+      var @base = new ViewStudyBase();
+      var @object = new ViewStudy();
+
+      var rig = new RigObj();
+      @object.LoadStudyToRig(ref rig);
+    }
+    
+  }
+}
