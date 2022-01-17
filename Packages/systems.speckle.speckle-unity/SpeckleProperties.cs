8e2685f0e96a763974bb5ed61eb2d7547f48e66d
David Morgan <30870056+haitheredavid@users.noreply.github.com>
2022-01-17 11:39:46 -0600

    removed mono from speck props

--- Packages/systems.speckle.speckle-unity/SpeckleProperties.cs
+++ Packages/systems.speckle.speckle-unity/SpeckleProperties.cs
@@ -9,69 +9,63 @@ using UnityEngine;
 namespace Speckle.ConnectorUnity
 {
     /// <summary>
-    /// This class gets attached to GOs and is used to store Speckle's metadata when sending / receiving
+    ///   This class gets attached to GOs and is used to store Speckle's metadata when sending / receiving
     /// </summary>
     [Serializable]
-    public class SpeckleProperties : MonoBehaviour, ISerializationCallbackReceiver
+  public class SpeckleProperties : ISerializationCallbackReceiver
+  {
+
+    [SerializeField] [HideInInspector]
+    private string _serializedData = "";
+    private ObservableConcurrentDictionary<string, object> _data;
+
+    private bool _hasChanged;
+
+    public SpeckleProperties()
     {
-
-        [SerializeField, HideInInspector]
-        private string _serializedData = "";
-    
-        private bool _hasChanged;
-        private ObservableConcurrentDictionary<string, object> _data;
-
-        public IDictionary<string, object> Data
-        {
-            get => _data;
-            set
-            {
-                ((ICollection<KeyValuePair<string, object>>) _data).Clear();
-
-                foreach (var kvp in value)
-                {
-                    _data.Add(kvp.Key, kvp.Value);
-                }
-            }
-        }
-
-        public SpeckleProperties()
-        {
-            _data = new ObservableConcurrentDictionary<string, object>();
-            _data.CollectionChanged += CollectionChangeHandler;
-            _hasChanged = true;
-        }
-    
-        private void CollectionChangeHandler(object sender, NotifyCollectionChangedEventArgs e)
-        {
-            _hasChanged = true;
-        }
-
-        public void OnBeforeSerialize()
-        {
-            if (!_hasChanged) return;
-      
-            _serializedData = Operations.Serialize(new SpeckleData(Data));
-            _hasChanged = false;
-        }
-    
-        public void OnAfterDeserialize()
-        {
-            Base speckleData = Operations.Deserialize(_serializedData);
-            Data = speckleData.GetMembers();
-            _hasChanged = false;
-        }
-
-        [Serializable]
-        private class SpeckleData : Base
-        {
-            public SpeckleData(IDictionary<string, object> data)
-            {
-                foreach (var v in data)
-                {
-                    this[v.Key] = v.Value;
-                }
-            }
-        }
+      _data = new ObservableConcurrentDictionary<string, object>();
+      _data.CollectionChanged += CollectionChangeHandler;
+      _hasChanged = true;
     }
+
+    public IDictionary<string, object> Data
+    {
+      get => _data;
+      set
+      {
+        ((ICollection<KeyValuePair<string, object>>)_data).Clear();
+
+        foreach (var kvp in value) _data.Add(kvp.Key, kvp.Value);
+      }
+    }
+
+    public void OnBeforeSerialize()
+    {
+      if (!_hasChanged) return;
+
+      _serializedData = Operations.Serialize(new SpeckleData(Data));
+      _hasChanged = false;
+    }
+
+    public void OnAfterDeserialize()
+    {
+      var speckleData = Operations.Deserialize(_serializedData);
+      Data = speckleData.GetMembers();
+      _hasChanged = false;
+    }
+
+    private void CollectionChangeHandler(object sender, NotifyCollectionChangedEventArgs e)
+    {
+      _hasChanged = true;
+    }
+
+    [Serializable]
+    private class SpeckleData : Base
+    {
+      public SpeckleData(IDictionary<string, object> data)
+      {
+        foreach (var v in data) this[v.Key] = v.Value;
+      }
+    }
+  }
 }
