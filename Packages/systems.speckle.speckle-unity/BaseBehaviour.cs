c63fb9bea6f78d9082c7561a507676a3c4913d40
David Morgan <30870056+haitheredavid@users.noreply.github.com>
2022-01-17 11:37:51 -0600

    added base behaviour

---
+++ Packages/systems.speckle.speckle-unity/BaseBehaviour.cs
@@ -0,0 +1,14 @@
+﻿using Speckle.ConnectorUnity;
+using UnityEngine;
+
+namespace Objects.Converter.Unity
+{
+  /// <summary>
+  ///   A simple version of the object Base from Speckle that contains the speckle properties type
+  /// </summary>
+  public class BaseBehaviour : MonoBehaviour
+  {
+
+    public SpeckleProperties properties;
+  }
+}
