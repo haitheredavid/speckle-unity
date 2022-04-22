f7d377a09429cb30b400747aa0c772969df99e4c
David Morgan <30870056+haitheredavid@users.noreply.github.com>
2022-04-22 16:53:07 -0400

    Main connector functions bound to editor (still hacky :()

---
+++ Packages/systems.speckle.speckle-unity/Editor/SpeckleConnectorEditor.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Cysharp.Threading.Tasks;
+using Speckle.ConnectorUnity.GUI;
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Speckle.ConnectorUnity
+{
+  [CustomEditor(typeof(SpeckleConnector))]
+  public class SpeckleConnectorEditor : Editor
+  {
+
+    private DropdownField accounts, streams, branches, commits;
+    private SpeckleConnector obj;
+    private VisualElement root;
+    private VisualTreeAsset tree;
+
+    private void OnEnable()
+    {
+      tree = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Packages/systems.speckle.speckle-unity/GUI/SpeckleConnectorEditor.uxml");
+      obj = (SpeckleConnector)target;
+    }
+
+    public override VisualElement CreateInspectorGUI()
+    {
+      if (tree == null)
+        return base.CreateInspectorGUI();
+
+      root = new VisualElement();
+      tree.CloneTree(root);
+
+      root.Q<ObjectField>("stream").objectType = typeof(SpeckleStream);
+
+      accounts = SetDropDown("accounts", "accountIndex", obj.accounts.Format(), e => AccountChange(e).Forget());
+
+      streams = SetDropDown("streams", "streamIndex", obj.streams.Format(), e => StreamChange(e).Forget());
+      branches = SetDropDown("branches", "branchIndex", obj.branches.Format(), BranchChange);
+      commits = SetDropDown("commits", "commitIndex", obj.commits.Format(), CommitChange);
+
+      return root;
+    }
+
+    private DropdownField SetDropDown(string fieldName, string propName, IEnumerable<string> items, Action<ChangeEvent<string>> callback)
+    {
+      var dropDown = root.Q<DropdownField>(fieldName);
+      dropDown.choices = items.ToList();
+      dropDown.index = FindInt(propName);
+      dropDown.RegisterValueChangedCallback(callback.Invoke);
+      return dropDown;
+    }
+
+    private async UniTaskVoid AccountChange(ChangeEvent<string> evt)
+    {
+      var inputA = evt.newValue.ParseAccountEmail();
+      var inputB = evt.newValue.ParseAccountServer();
+
+      if (obj == null)
+        return;
+
+      var index = -1;
+      for (var i = 0; i < obj.accounts.Count; i++)
+      {
+        var item = obj.accounts[i];
+        if (item != null && item.userInfo.email.Equals(inputA) && item.serverInfo.name.Equals(inputB))
+        {
+          Debug.Log($"Setting active {nameof(item)} to {inputA}-{inputB}");
+          index = i;
+          break;
+        }
+      }
+
+      if (index < 0)
+        return;
+
+      await obj.LoadAccount(index);
+      RefreshAll();
+    }
+
+    private async UniTask StreamChange(ChangeEvent<string> evt)
+    {
+      var itemA = evt.newValue.ParseStreamName();
+      var itemB = evt.newValue.ParseStreamId();
+      var index = -1;
+
+      for (var i = 0; i < obj.streams.Count; i++)
+      {
+        var item = obj.streams[i];
+        if (item != null && item.name.Equals(itemA) && item.id.Equals(itemB))
+        {
+          Debug.Log($"Setting active {nameof(item)} to {itemA} | {itemB}");
+          index = i;
+          break;
+        }
+      }
+
+      if (index < 0)
+        return;
+
+      await obj.LoadStream(index);
+
+      Refresh(streams, obj.streams.Format(), "streamIndex");
+      Refresh(branches, obj.branches.Format(), "branchIndex");
+      Refresh(commits, obj.commits.Format(), "commitIndex");
+    }
+
+    private void BranchChange(ChangeEvent<string> evt)
+    {
+      Debug.Log(evt.newValue + $" from {evt.previousValue}");
+      var itemA = evt.newValue;
+      for (var i = 0; i < obj.branches.Count; i++)
+      {
+        var item = obj.branches[i];
+        if (item != null && item.name.Equals(itemA))
+        {
+          obj.LoadBranch(i);
+          break;
+        }
+      }
+
+      Refresh(commits, obj.commits.Format(), "commitIndex");
+    }
+
+    private void CommitChange(ChangeEvent<string> evt)
+    {
+      var itemA = evt.newValue.ParseCommitId();
+      var itemB = evt.newValue.ParseCommitMsg();
+
+      for (var i = 0; i < obj.commits.Count; i++)
+      {
+        var item = obj.commits[i];
+        if (item != null && item.id.Equals(itemA) && item.message.Equals(itemB))
+        {
+          Debug.Log($"Setting active commit to {itemA} | {itemB}");
+          obj.LoadCommit(i);
+          break;
+        }
+      }
+    }
+
+    private int FindInt(string propName)
+    {
+      return serializedObject.FindProperty(propName).intValue;
+    }
+
+    private void RefreshAll()
+    {
+      Refresh(accounts, obj.accounts.Format(), "accountIndex");
+      Refresh(streams, obj.streams.Format(), "streamIndex");
+      Refresh(branches, obj.branches.Format(), "branchIndex");
+      Refresh(commits, obj.commits.Format(), "commitIndex");
+    }
+
+    private void Refresh(DropdownField dropdown, IEnumerable<string> items, string prop)
+    {
+      dropdown.choices = items.ToList();
+      dropdown.index = FindInt(prop);
+    }
+  }
+}
--- Packages/systems.speckle.speckle-unity/SpeckleConnector.cs
+++ Packages/systems.speckle.speckle-unity/SpeckleConnector.cs
@@ -1,20 +1,57 @@
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Cysharp.Threading.Tasks;
+using Speckle.Core.Api;
+using Speckle.Core.Credentials;
+using Speckle.Core.Logging;
 using UnityEngine;
 
 namespace Speckle.ConnectorUnity
 {
+
   [AddComponentMenu("Speckle/Speckle Connector")]
+  [ExecuteAlways]
   public class SpeckleConnector : MonoBehaviour
   {
     [SerializeField] private ConverterUnity converter;
+
     [SerializeField] private List<Sender> senders = new List<Sender>();
     [SerializeField] private List<Receiver> receivers = new List<Receiver>();
 
-    private void Start()
-    {
-      Receive().Forget();
+    [SerializeField] private SpeckleStream stream;
 
+    [SerializeField] private int accountIndex;
+    [SerializeField] private int streamIndex;
+    [SerializeField] private int branchIndex;
+    [SerializeField] private int commitIndex;
+    [SerializeField] private int kitIndex;
+
+    private Client client;
+
+    public List<Account> accounts { get; set; }
+    public List<Stream> streams { get; private set; }
+    public List<Branch> branches { get; private set; }
+    public List<Commit> commits { get; private set; }
+
+    public Account activeAccount
+    {
+      get => accounts.Valid(accountIndex) ? accounts[accountIndex] : null;
+    }
+
+    public Stream activeStream
+    {
+      get => streams.Valid(streamIndex) ? streams[streamIndex] : null;
+    }
+
+    public Branch activeBranch
+    {
+      get => branches.Valid(branchIndex) ? branches[branchIndex] : null;
+    }
+
+    public Commit activeCommit
+    {
+      get => commits.Valid(commitIndex) ? commits[commitIndex] : null;
     }
 
     private void OnEnable()
@@ -22,6 +59,97 @@ namespace Speckle.ConnectorUnity
       senders ??= new List<Sender>();
       receivers ??= new List<Receiver>();
 
+      accounts = AccountManager.GetAccounts().ToList();
+      LoadAccount().Forget();
+    }
+
+    //
+    // private void Start()
+    // {
+    //   Receive().Forget();
+    // }
+
+    public async UniTask LoadAccount(int index = -1)
+    {
+      try
+      {
+        if (accounts == null)
+        {
+          ConnectorConsole.Warn("Accounts are not set properly to this connector");
+          return;
+        }
+
+        client = null;
+        streamIndex = 0;
+
+        accountIndex = Check(accounts, index);
+
+        if (activeAccount != null)
+        {
+          client = new Client(activeAccount);
+          streams = await client.StreamsGet();
+        }
+      }
+      catch (SpeckleException e)
+      {
+        ConnectorConsole.Warn(e.Message);
+        streams = new List<Stream>();
+      }
+      finally
+      {
+        await LoadStream(streamIndex);
+      }
+    }
+
+    public async UniTask LoadStream(int index = -1)
+    {
+      try
+      {
+        branchIndex = 0;
+        commitIndex = 0;
+
+        if (client == null && activeAccount != null)
+          client = new Client(activeAccount);
+
+        streamIndex = Check(streams, index);
+
+        if (activeStream != null)
+          branches = await client.StreamGetBranches(activeStream.id, 20, 20);
+
+        if (branches != null)
+        {
+          for (int bIndex = 0; bIndex < branches.Count; bIndex++)
+          {
+            if (branches[bIndex].name.Equals("main"))
+            {
+              LoadBranch(bIndex);
+              break;
+            }
+          }
+        }
+      }
+      catch (SpeckleException e)
+      {
+        ConnectorConsole.Warn(e.Message);
+        branches = new List<Branch>();
+      }
+    }
+
+    public void LoadBranch(int i = 0)
+    {
+      branchIndex = Check(branches, i);
+      Debug.Log($"Loading new branch {activeBranch}");
+
+      commits = activeBranch != null ? activeBranch.commits.items : new List<Commit>();
+      LoadCommit();
+    }
+
+    public void LoadCommit(int i = 0)
+    {
+      commitIndex = Check(commits, i);
+
+      if (activeCommit != null)
+        ConnectorConsole.Log("Active commit loaded! " + activeCommit);
     }
 
     public async UniTaskVoid Receive()
@@ -41,6 +169,13 @@ namespace Speckle.ConnectorUnity
       var sender = new GameObject().AddComponent<Sender>();
       // await sender.Init("https://speckle.xyz/streams/4f5b4785b0/");
       // await sender.Send(new List<GameObject>() { o });
+    }
+
+    private static int Check(IList list, int index)
+    {
+      var res = list.Valid(index) ? index : 0;
+      Debug.Log($"checking for index {index} and returning {res}");
+      return res;
     }
   }
 
