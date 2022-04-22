08b7aaf5f27e803e12bb3d3c31c1b97af16ae0e6
David Morgan <30870056+haitheredavid@users.noreply.github.com>
2022-04-22 16:52:20 -0400

    Drafted Speckle Stream Editor and Element

---
+++ Packages/systems.speckle.speckle-unity/GUI/Elements/StreamThumbnail.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine.UIElements;
+
+namespace Speckle.ConnectorUnity.GUI
+{
+
+  public class StreamThumbnail : VisualElement
+  {
+
+    public Image thumbnail;
+
+    public StreamThumbnail()
+    {
+      thumbnail = new Image();
+      Add(thumbnail);
+    }
+  }
+}
