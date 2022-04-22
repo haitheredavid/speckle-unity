d1d1ebd08635cb8a89892a14eb071242994f5d5d
David Morgan <30870056+haitheredavid@users.noreply.github.com>
2022-04-22 16:51:05 -0400

    brought back in GUI utils

---
+++ Packages/systems.speckle.speckle-unity/RandomUtils.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+
+namespace Speckle.ConnectorUnity
+{
+  public static class RandomUtils
+  {
+
+    public static bool Valid(this IList list) => list.Valid(0);
+
+    public static bool Valid(this IList list, int count) => list != null && count >= 0 && count < list.Count;
+
+    public static bool Valid(this ICollection list) => list.Valid(0);
+
+    public static bool Valid(this ICollection list, int count) => list != null && count >= 0 && count < list.Count;
+  }
+}
