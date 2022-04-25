1c9b549bc1c22ba1eb9a87b4dcef3a729a019037
David Morgan <30870056+haitheredavid@users.noreply.github.com>
2022-04-25 16:32:54 -0400

    clean up

--- Packages/systems.speckle.speckle-unity/SpeckleConnector.cs
+++ Packages/systems.speckle.speckle-unity/SpeckleConnector.cs
@@ -35,29 +35,34 @@ namespace Speckle.ConnectorUnity
 
     private Client client;
 
-    public List<Account> accounts { get; set; }
-    public List<Stream> streams { get; private set; }
-    public List<Branch> branches { get; private set; }
-    public List<Commit> commits { get; private set; }
+    public List<Account> Accounts { get; private set; }
+    public List<Stream> Streams { get; private set; }
+    public List<Branch> Branches { get; private set; }
+    public List<Commit> Commits { get; private set; }
+
+    public List<ConverterUnity> Converters
+    {
+      get => converters.Valid() ? converters : new List<ConverterUnity>();
+    }
 
     public Account activeAccount
     {
-      get => accounts.Valid(accountIndex) ? accounts[accountIndex] : null;
+      get => Accounts.Valid(accountIndex) ? Accounts[accountIndex] : null;
     }
 
     public Stream activeStream
     {
-      get => streams.Valid(streamIndex) ? streams[streamIndex] : null;
+      get => Streams.Valid(streamIndex) ? Streams[streamIndex] : null;
     }
 
     public Branch activeBranch
     {
-      get => branches.Valid(branchIndex) ? branches[branchIndex] : null;
+      get => Branches.Valid(branchIndex) ? Branches[branchIndex] : null;
     }
 
     public Commit activeCommit
     {
-      get => commits.Valid(commitIndex) ? commits[commitIndex] : null;
+      get => Commits.Valid(commitIndex) ? Commits[commitIndex] : null;
     }
 
     private void OnEnable()
@@ -92,7 +97,7 @@ namespace Speckle.ConnectorUnity
     {
       try
       {
-        if (accounts == null)
+        if (Accounts == null)
         {
           ConnectorConsole.Warn("Accounts are not set properly to this connector");
           return;
@@ -101,18 +106,18 @@ namespace Speckle.ConnectorUnity
         client = null;
         streamIndex = 0;
 
-        accountIndex = Check(accounts, index);
+        accountIndex = Check(Accounts, index);
 
         if (activeAccount != null)
         {
           client = new Client(activeAccount);
-          streams = await client.StreamsGet();
+          Streams = await client.StreamsGet();
         }
       }
       catch (SpeckleException e)
       {
         ConnectorConsole.Warn(e.Message);
-        streams = new List<Stream>();
+        Streams = new List<Stream>();
       }
       finally
       {
@@ -127,27 +132,27 @@ namespace Speckle.ConnectorUnity
       try
       {
         branchIndex = 0;
-        branches = new List<Branch>();
+        Branches = new List<Branch>();
 
         commitIndex = 0;
-        commits = new List<Commit>();
+        Commits = new List<Commit>();
 
 
         if (client == null && activeAccount != null)
           client = new Client(activeAccount);
 
-        streamIndex = Check(streams, index);
+        streamIndex = Check(Streams, index);
 
         if (activeStream != null)
         {
-          branches = await client.StreamGetBranches(activeStream.id, 20, 20);
+          Branches = await client.StreamGetBranches(activeStream.id, 20, 20);
         }
 
-        if (branches != null)
+        if (Branches != null)
         {
-          for (int bIndex = 0; bIndex < branches.Count; bIndex++)
+          for (int bIndex = 0; bIndex < Branches.Count; bIndex++)
           {
-            if (branches[bIndex].name.Equals("main"))
+            if (Branches[bIndex].name.Equals("main"))
             {
               LoadBranch(bIndex);
               break;
@@ -158,21 +163,21 @@ namespace Speckle.ConnectorUnity
       catch (SpeckleException e)
       {
         ConnectorConsole.Warn(e.Message);
-        branches = new List<Branch>();
+        Branches = new List<Branch>();
       }
     }
 
     public void LoadBranch(int i = -1)
     {
-      branchIndex = Check(branches, i);
+      branchIndex = Check(Branches, i);
 
-      commits = activeBranch != null ? activeBranch.commits.items : new List<Commit>();
+      Commits = activeBranch != null ? activeBranch.commits.items : new List<Commit>();
       LoadCommit();
     }
 
     public void LoadCommit(int i = -1)
     {
-      commitIndex = Check(commits, i);
+      commitIndex = Check(Commits, i);
 
       if (activeCommit != null)
         ConnectorConsole.Log("Active commit loaded! " + activeCommit);
