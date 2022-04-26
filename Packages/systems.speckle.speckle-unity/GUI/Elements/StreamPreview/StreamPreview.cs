37a8fab6a5128c5ab7ed0b994b80eb1e43550638
David Morgan <30870056+haitheredavid@users.noreply.github.com>
2022-04-26 08:43:23 -0400

    basic card and element

---
+++ Packages/systems.speckle.speckle-unity/GUI/Elements/StreamPreview/StreamPreview.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine.Scripting;
+using UnityEngine.UIElements;
+
+namespace Speckle.ConnectorUnity.GUI
+{
+
+  public class StreamPreview : VisualElement
+  {
+
+    public Image thumbnail;
+
+    public StreamPreview()
+    {
+
+      thumbnail = new Image();
+      Add(thumbnail);
+
+      thumbnail.AddToClassList("previewIcon");
+      AddToClassList("previewContainer");
+    }
+
+    #region UXML
+    [Preserve]
+    public new class UxmlFactory : UxmlFactory<StreamPreview, UxmlTraits>
+    { }
+
+    [Preserve]
+    public new class UxmlTraits : VisualElement.UxmlTraits
+    { }
+    #endregion
+
+  }
+}
