decf7ab0b179cfc5870fb05b1724d6e4fff8d0d3
David Morgan <30870056+haitheredavid@users.noreply.github.com>
2022-04-28 00:57:16 -0400

    Cleaned up Sender.cs.

--- Packages/systems.speckle.speckle-unity/Ops/Sender.cs
+++ Packages/systems.speckle.speckle-unity/Ops/Sender.cs
@@ -3,43 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using Cysharp.Threading.Tasks;
-using Sentry;
 using Speckle.Core.Api;
 using Speckle.Core.Logging;
 using Speckle.Core.Models;
+using Speckle.Core.Transports;
 using UnityEngine;
 using UnityEngine.Events;
 
 namespace Speckle.ConnectorUnity
 {
 
-  /// <summary>
-  ///   A Speckle Sender, it's a wrapper around a basic Speckle Client
-  ///   that handles conversions for you
-  /// </summary>
-  [AddComponentMenu("Speckle/Sender")]
-  public class Sender : SpeckleClient
-  {
+	/// <summary>
+	///   A Speckle Sender, it's a wrapper around a basic Speckle Client
+	///   that handles conversions for you
+	/// </summary>
+	[AddComponentMenu("Speckle/Sender")]
+	public class Sender : SpeckleClient
+	{
+		public UnityAction<string> onDataSent;
 
 		private ServerTransport transport;
 
-    public async UniTask<string> Send(List<GameObject> objs = null, string message = null, CancellationTokenSource cancellationToken = null)
-    {
+		public async UniTask<string> Send(List<GameObject> objs = null, string message = null, CancellationTokenSource cancellationToken = null)
+		{
+			var objectId = "";
 
-      var objectId = "";
+			if (!IsReady())
+				return objectId;
 
-      if (!IsReady())
-        return objectId;
+			// if no objects were passed in we'll try converting from the assigned root object 
+			if (objs == null || !objs.Any())
+				objs = new List<GameObject>() { root };
 
-      // if no objects were passed in we'll try converting from the assigned root object 
-      if (objs == null || !objs.Any())
-        objs = new List<GameObject>() { root };
+			var data = objs.Count > 1 ? ConvertRecursively(objs) : ConvertRecursively(objs[0]);
 
-      var data = objs.Count > 1 ? ConvertRecursively(objs) : ConvertRecursively(objs[0]);
-
-      try
-      {
-        ConnectorConsole.Log("Sending data");
+			try
+			{
+				ConnectorConsole.Log("Sending data");
 
 				transport = new ServerTransport(client.Account, stream.Id);
 
@@ -81,50 +81,54 @@ namespace Speckle.ConnectorUnity
 			return objectId;
 		}
 
-    }
+		protected override void CleanUp()
+		{
+			base.CleanUp();
+			transport?.Dispose();
+		}
 
-    #region private methods
-    private Base ConvertRecursively(IEnumerable<GameObject> objs)
-    {
-      return new Base()
-      {
-        ["objects"] = objs.Select(ConvertRecursively).Where(x => x != null).ToList()
-      };
-    }
+		#region private methods
+		private Base ConvertRecursively(IEnumerable<GameObject> objs)
+		{
+			return new Base()
+			{
+				["objects"] = objs.Select(ConvertRecursively).Where(x => x != null).ToList()
+			};
+		}
 
-    private Base ConvertRecursively(GameObject go)
-    {
-      if (converter.CanConvertToSpeckle(go))
-        try
-        {
-          return converter.ConvertToSpeckle(go);
-        }
-        catch (Exception e)
-        {
-          Debug.LogException(e);
-        }
+		private Base ConvertRecursively(GameObject go)
+		{
+			if (converter.CanConvertToSpeckle(go))
+				try
+				{
+					return converter.ConvertToSpeckle(go);
+				}
+				catch (Exception e)
+				{
+					Debug.LogException(e);
+				}
 
-      return CheckForChildren(go, out var objs) ?
-        new Base { ["objects"] = objs } : null;
-    }
+			return CheckForChildren(go, out var objs) ?
+				new Base { ["objects"] = objs } : null;
+		}
 
-    private bool CheckForChildren(GameObject go, out List<Base> objs)
-    {
-      objs = new List<Base>();
+		private bool CheckForChildren(GameObject go, out List<Base> objs)
+		{
+			objs = new List<Base>();
 
-      if (go != null && go.transform.childCount > 0)
-      {
-        foreach (Transform child in go.transform)
-        {
-          var converted = ConvertRecursively(child.gameObject);
-          if (converted != null)
-            objs.Add(converted);
-        }
-      }
+			if (go != null && go.transform.childCount > 0)
+			{
+				foreach (Transform child in go.transform)
+				{
+					var converted = ConvertRecursively(child.gameObject);
+					if (converted != null)
+						objs.Add(converted);
+				}
+			}
 
-      return objs.Any();
-    }
-    #endregion
+			return objs.Any();
+		}
+		#endregion
 
-  }
+	}
 }
