d6e63cd2ea04d5181b1e7772d6efe303f3f9f490
David Morgan <30870056+haitheredavid@users.noreply.github.com>
2023-05-09 18:55:34 -0400

    Migrated some utilities

---
+++ Packages/com.speckle.core.component-conveter/Runtime/Utilities/ConverterUtils.Mesh.cs
@@ -0,0 +1,318 @@
+﻿using Objects.Geometry;
+using Objects.Other;
+using Objects.Utils;
+using System.Collections.Generic;
+using System.Linq;
+using Speckle.ConnectorUnity.Core.ScriptableConverter.Components;
+using UnityEngine;
+using UnityEngine.Rendering;
+using SMesh = Objects.Geometry.Mesh;
+
+namespace Speckle.ConnectorUnity.Core.ScriptableConverter
+{
+
+
+    public static partial class ConverterUtils
+    {
+
+        public static bool IsRuntime => Application.isPlaying;
+
+
+
+        public static SMesh MeshToSpeckle(this IMeshableConverter converter, MeshFilter component)
+        {
+            var nativeMesh = IsRuntime ? component.mesh : component.sharedMesh;
+
+            var nTriangles = nativeMesh.triangles;
+            var sFaces = new List<int>(nTriangles.Length * 4);
+            for (var i = 2; i < nTriangles.Length; i += 3)
+            {
+                sFaces.Add(0); //Triangle cardinality indicator
+
+                sFaces.Add(nTriangles[i]);
+                sFaces.Add(nTriangles[i - 1]);
+                sFaces.Add(nTriangles[i - 2]);
+            }
+
+            var nVertices = nativeMesh.vertices;
+            var sVertices = new List<double>(nVertices.Length * 3);
+
+            foreach (var vertex in nVertices)
+            {
+                var p = component.gameObject.transform.TransformPoint(vertex);
+                sVertices.Add(p.x);
+                sVertices.Add(p.z); //z and y swapped
+                sVertices.Add(p.y);
+            }
+
+            var nColors = nativeMesh.colors;
+            var sColors = new List<int>(nColors.Length);
+            sColors.AddRange(nColors.Select(c => c.ToIntColor()));
+
+            var nTexCoords = nativeMesh.uv;
+            var sTexCoords = new List<double>(nTexCoords.Length * 2);
+            foreach (var uv in nTexCoords)
+            {
+                sTexCoords.Add(uv.x);
+                sTexCoords.Add(uv.y);
+            }
+
+            // NOTE: this throws some exceptions with trying to set a method that isn't settable.
+            // Looking at other converters it seems like the conversion code should be handling all the prop settings..
+
+            //
+            // // get the speckle data from the go here
+            // // so that if the go comes from speckle, typed props will get overridden below
+            // // TODO: Maybe handle a better way of overriding props? Or maybe this is just the typical logic for connectors 
+            // if (convertProps)
+            // {
+            //   // Base behaviour is the standard unity mono type that stores the speckle props data
+            //   var baseBehaviour = component.GetComponent(typeof(BaseBehaviour)) as BaseBehaviour;
+            //   if (baseBehaviour != null && baseBehaviour.properties != null)
+            //   {
+            //     baseBehaviour.properties.AttachUnityProperties(mesh, excludedProps);
+            //   }
+            // }
+
+            return new SMesh
+            {
+                vertices = sVertices,
+                faces = sFaces,
+                colors = sColors,
+                textureCoordinates = sTexCoords,
+                units = ModelUnits
+            };
+        }
+
+        public static void MeshToNative(this IMeshableConverter converter, SMesh mesh, GameObject obj)
+        {
+            if (mesh == null || mesh.vertices.Count == 0 || mesh.faces.Count == 0) return;
+
+            var data = new SpeckleMesh.Data
+            {
+                uvs = new List<Vector2>(),
+                vertices = new List<Vector3>(),
+                subMeshes = new List<List<int>>(),
+                vertexColors = new List<Color>()
+            };
+
+            var nativeMesh = new UnityEngine.Mesh();
+
+            data.AddMesh(mesh);
+
+            nativeMesh.SetVertices(data.vertices);
+            nativeMesh.SetUVs(0, data.uvs);
+            nativeMesh.SetColors(data.vertexColors);
+
+            var j = 0;
+            foreach (var subMeshTriangles in data.subMeshes)
+            {
+                nativeMesh.SetTriangles(subMeshTriangles, j);
+                j++;
+            }
+
+            if (nativeMesh.vertices.Length >= ushort.MaxValue)
+                nativeMesh.indexFormat = IndexFormat.UInt32;
+
+            nativeMesh.Optimize();
+            nativeMesh.RecalculateBounds();
+            nativeMesh.RecalculateNormals();
+            nativeMesh.RecalculateTangents();
+
+            nativeMesh.subMeshCount = data.subMeshes.Count;
+
+            var filter = obj.GetComponent<MeshFilter>();
+
+            if (filter == null)
+                filter = obj.AddComponent<MeshFilter>();
+
+            if (IsRuntime)
+                filter.mesh = nativeMesh;
+            else
+                filter.sharedMesh = nativeMesh;
+
+            if (converter.addMeshCollider)
+            {
+                var c = filter.gameObject.GetComponent<MeshCollider>();
+                if (c == null) c = filter.gameObject.AddComponent<MeshCollider>();
+
+                c.sharedMesh = IsRuntime ? filter.mesh : filter.sharedMesh;
+            }
+
+            if (converter.addMeshRenderer)
+            {
+                var c = filter.gameObject.GetComponent<MeshRenderer>();
+                if (c == null) c = filter.gameObject.AddComponent<MeshRenderer>();
+
+                // c.sharedMaterial = converter.useRenderMaterial ?
+                //   GetMaterial(converter, mesh["renderMaterial"] as RenderMaterial) :
+                //   converter.defaultMaterial;
+            }
+        }
+
+
+        public static GameObject MeshToNative(this IMeshableConverter converter, IReadOnlyCollection<SMesh> meshes, GameObject obj)
+        {
+            var materials = new List<UnityEngine.Material>(meshes.Count);
+
+            var data = new SpeckleMesh.Data
+            {
+                uvs = new List<Vector2>(),
+                vertices = new List<Vector3>(),
+                subMeshes = new List<List<int>>(),
+                vertexColors = new List<Color>()
+            };
+
+            foreach (var speckleMesh in meshes)
+            {
+                if (speckleMesh.vertices.Count == 0 || speckleMesh.faces.Count == 0) continue;
+
+                data.AddMesh(speckleMesh);
+                // // Convert RenderMaterial
+                // materials.Add(converter.useRenderMaterial ?
+                //   GetMaterial(converter, speckleMesh["renderMaterial"] as RenderMaterial) :
+                //   converter.defaultMaterial
+                // );
+            }
+
+            var nativeMaterials = materials.ToArray();
+
+            var nativeMesh = new UnityEngine.Mesh {subMeshCount = data.subMeshes.Count};
+
+            nativeMesh.SetVertices(data.vertices);
+            nativeMesh.SetUVs(0, data.uvs);
+            nativeMesh.SetColors(data.vertexColors);
+
+            var j = 0;
+            foreach (var subMeshTriangles in data.subMeshes)
+            {
+                nativeMesh.SetTriangles(subMeshTriangles, j);
+                j++;
+            }
+
+            if (nativeMesh.vertices.Length >= ushort.MaxValue)
+                nativeMesh.indexFormat = IndexFormat.UInt32;
+
+            nativeMesh.Optimize();
+            nativeMesh.RecalculateBounds();
+            nativeMesh.RecalculateNormals();
+            nativeMesh.RecalculateTangents();
+
+            // Debug.Log($"Mesh Stats for Native"
+            //           + $"\nVertexCount:{nativeMesh.vertexCount}"
+            //           + $"\nSubMeshes:{nativeMesh.subMeshCount}");
+
+            var filter = obj.GetComponent<MeshFilter>();
+
+            if (filter == null)
+                filter = obj.AddComponent<MeshFilter>();
+
+            if (IsRuntime)
+                filter.mesh = nativeMesh;
+            else
+                filter.sharedMesh = nativeMesh;
+
+            if (converter.addMeshCollider)
+                filter.gameObject.AddComponent<MeshCollider>().sharedMesh = IsRuntime ? filter.mesh : filter.sharedMesh;
+
+            if (converter.addMeshRenderer)
+                filter.gameObject.AddComponent<MeshRenderer>().sharedMaterials = nativeMaterials;
+
+            return obj;
+        }
+
+        public static void AddMesh(this SpeckleMesh.Data data, SMesh subMesh)
+        {
+            subMesh.AlignVerticesWithTexCoordsByIndex();
+            subMesh.TriangulateMesh();
+
+            var indexOffset = data.vertices.Count;
+
+            // Convert Vertices
+            data.vertices.AddRange(subMesh.vertices.ArrayToVector3(subMesh.units));
+
+            // Convert texture coordinates
+            var hasValidUVs = subMesh.TextureCoordinatesCount == subMesh.VerticesCount;
+            if (subMesh.textureCoordinates.Count > 0 && !hasValidUVs)
+                Debug.LogWarning(
+                    $"Expected number of UV coordinates to equal vertices. Got {subMesh.TextureCoordinatesCount} expected {subMesh.VerticesCount}. \nID = {subMesh.id}");
+
+            if (hasValidUVs)
+            {
+                data.uvs.Capacity += subMesh.TextureCoordinatesCount;
+                for (var j = 0; j < subMesh.TextureCoordinatesCount; j++)
+                {
+                    var (u, v) = subMesh.GetTextureCoordinate(j);
+                    data.uvs.Add(new Vector2((float)u, (float)v));
+                }
+            }
+            else if (subMesh.bbox != null)
+            {
+                //Attempt to generate some crude UV coordinates using bbox
+                ////TODO this will be broken for submeshes
+                data.uvs.AddRange(subMesh.bbox.GenerateUV(data.vertices));
+            }
+
+            // Convert vertex colors
+            if (subMesh.colors != null)
+            {
+                if (subMesh.colors.Count == subMesh.VerticesCount)
+                    data.vertexColors.AddRange(subMesh.colors.Select(c => c.ToUnityColor()));
+                else if (subMesh.colors.Count != 0)
+                    //TODO what if only some submeshes have colors?
+                    Debug.LogWarning(
+                        $"{typeof(SMesh)} {subMesh.id} has invalid number of vertex {nameof(SMesh.colors)}. Expected 0 or {subMesh.VerticesCount}, got {subMesh.colors.Count}");
+            }
+
+            var tris = new List<int>();
+
+            // Convert faces
+            tris.Capacity += (int)(subMesh.faces.Count / 4f) * 3;
+
+            // skip the 0 index and then only grab every 3 
+            for (var i = 0; i < subMesh.faces.Count; i += 4)
+            {
+                //We can safely assume all faces are triangles since we called TriangulateMesh
+                tris.Add(subMesh.faces[i + 1] + indexOffset);
+                tris.Add(subMesh.faces[i + 3] + indexOffset);
+                tris.Add(subMesh.faces[i + 2] + indexOffset);
+            }
+
+            data.subMeshes.Add(tris);
+        }
+
+        public static IEnumerable<Vector2> GenerateUV(this Box bbox, IReadOnlyList<Vector3> verts)
+        {
+            var uv = new Vector2[verts.Count];
+            var xSize = (float)bbox.xSize.Length;
+            var ySize = (float)bbox.ySize.Length;
+
+            for (var i = 0; i < verts.Count; i++)
+            {
+                var vert = verts[i];
+                uv[i] = new Vector2(vert.x / xSize, vert.y / ySize);
+            }
+
+            return uv;
+        }
+
+        public static void RecenterVertices(List<Vector3> vertices, out Vector3 center)
+        {
+            center = Vector3.zero;
+
+            if (vertices == null || !vertices.Any()) return;
+
+            Bounds meshBounds = new Bounds {center = vertices[0]};
+
+            foreach (var vert in vertices)
+                meshBounds.Encapsulate(vert);
+
+            center = meshBounds.center;
+
+            for (int i = 0; i < vertices.Count; i++)
+                vertices[i] -= meshBounds.center;
+        }
+
+    }
+
+}
---
+++ Packages/com.speckle.core.component-conveter/Runtime/Utilities/ConverterUtils.Point.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Objects.Geometry;
+using Unity.Collections;
+using UnityEngine;
+
+namespace Speckle.ConnectorUnity.Core.ScriptableConverter
+{
+	public static partial class ConverterUtils
+
+	{
+		/// <summary>
+		///   switch y and z
+		/// </summary>
+		/// <param name="x"></param>
+		/// <param name="y"></param>
+		/// <param name="z"></param>
+		/// <param name="units"></param>
+		/// <returns></returns>
+		public static Vector3 VectorByCoordinates(double x, double y, double z, string units)
+		{
+			return new((float)ScaleToNative(x, units), (float)ScaleToNative(z, units), (float)ScaleToNative(y, units));
+		}
+		//
+		// public static float3 Float3ByCoordinates(double x, double y, double z, string units)
+		// {
+		// 	return new((float)ScaleToNative(x, units), (float)ScaleToNative(z, units), (float)ScaleToNative(y, units));
+		// }
+
+		public static Vector ToSpeckle(this Vector3 pos, bool flipYZ = true) => flipYZ ? new Vector(pos.x, pos.z, pos.y) : new Vector(pos.x, pos.y, pos.z);
+
+		public static List<double> ToSpeckle(this IEnumerable<Vector3> points)
+		{
+			var res = new List<double>();
+
+			if (points == null)
+			{
+				Debug.LogException(new Exception("point array is not valid "));
+				return res;
+			}
+
+			foreach (var point in points)
+			{
+				res.Add(point.x);
+				res.Add(point.y);
+				res.Add(point.z);
+			}
+
+			return res;
+		}
+
+		public static Point ToPoint(this Vector3 pos, bool flipYZ = true) => flipYZ ? new Point(pos.x, pos.z, pos.y) : new Point(pos.x, pos.y, pos.z);
+
+		// /// <summary>
+		// /// 
+		// /// </summary>
+		// /// <param name="verts"></param>
+		// /// <param name="units"></param>
+		// /// <returns></returns>
+		// public static UniTask<IEnumerable<Vector3>> ArrayToVector3Task(this IEnumerable<double> verts, string units)
+		// {
+		// 	var src = new UniTaskCompletionSource<IEnumerable<Vector3>>();
+		// 	src.TrySetResult(verts.ArrayToVector3(units));
+		// 	return src.Task;
+		// }
+		//
+		// public static NativeArray<float3> ArrayToNativeArray(this IEnumerable<double> verts, string units)
+		// {
+		// 	if (!verts.FormatCorrect())
+		// 	{
+		// 		throw new Exception("point array is not valid ");
+		// 	}
+		//
+		// 	var asArray = verts.ToArray();
+		// 	var points = new NativeArray<float3>(asArray.Count() / 3, Allocator.Temp);
+		//
+		// 	for (int i = 2, k = 0; i < asArray.Count(); i += 3)
+		// 		points[k++] = Float3ByCoordinates(asArray[i - 2], asArray[i - 1], asArray[i], units);
+		//
+		// 	return points;
+		// }
+
+		static bool FormatCorrect(this IEnumerable<double> verts)
+		{
+			if (verts == null || verts.Count() % 3 != 0)
+			{
+				SpeckleUnity.Console.Warn("point array is not valid ");
+				return false;
+			}
+
+			return true;
+		}
+
+		public static IEnumerable<Vector3> ArrayToVector3(this IEnumerable<double> arr, string units)
+		{
+			if (arr == null)
+				throw new Exception("point array is not valid ");
+
+			if (arr.Count() % 3 != 0)
+				throw new Exception("Array malformed: length%3 != 0.");
+
+			var points = new Vector3[arr.Count() / 3];
+			var asArray = arr.ToArray();
+			for (int i = 2, k = 0; i < arr.Count(); i += 3)
+				points[k++] = VectorByCoordinates(asArray[i - 2], asArray[i - 1], asArray[i], units);
+
+			return points;
+		}
+
+		public static IEnumerable<Vector3> ArrayToVector3(this IReadOnlyCollection<Point> arr, string units)
+		{
+			if (arr == null)
+				throw new Exception("point array is not valid ");
+
+			var points = new Vector3[arr.Count()];
+			var asArray = arr.ToArray();
+
+			for (var i = 0; i < points.Count(); i++)
+				points[i] = asArray[i].ToVector3();
+
+			return points;
+		}
+
+		/// <summary>
+		/// </summary>
+		/// <param name="x"></param>
+		/// <param name="y"></param>
+		/// <param name="z"></param>
+		/// <param name="units"></param>
+		/// <param name="flipYZ"></param>
+		/// <returns></returns>
+		public static Vector3 ToVector3(double x, double y, double z, string units, bool flipYZ = true) =>
+			// switch y and z
+			flipYZ ?
+				new Vector3(
+					(float)ScaleToNative(x, units),
+					(float)ScaleToNative(z, units),
+					(float)ScaleToNative(y, units)) :
+				new Vector3(
+					(float)ScaleToNative(x, units),
+					(float)ScaleToNative(y, units),
+					(float)ScaleToNative(z, units));
+
+		public static Vector3 ToVector3(this Point p, bool flipYZ = true) => flipYZ ?
+			new Vector3(
+				(float)ScaleToNative(p.x, p.units),
+				(float)ScaleToNative(p.z, p.units),
+				(float)ScaleToNative(p.y, p.units))
+			:
+			new Vector3(
+				(float)ScaleToNative(p.x, p.units),
+				(float)ScaleToNative(p.y, p.units),
+				(float)ScaleToNative(p.z, p.units));
+	}
+}
