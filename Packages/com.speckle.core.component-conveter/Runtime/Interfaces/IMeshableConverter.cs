3c755e328db9fd44eb1bc94588b442590cbc59d3
David Morgan <30870056+haitheredavid@users.noreply.github.com>
2023-05-09 18:54:38 -0400

    Brought in previous Interfaces.meta

---
+++ Packages/com.speckle.core.component-conveter/Runtime/Interfaces/IMeshableConverter.cs
@@ -0,0 +1,54 @@
+﻿namespace Speckle.ConnectorUnity.Core.ScriptableConverter
+{
+
+
+    public interface IMeshableConverter
+    {
+        /// <summary>
+        /// Adds mesh colliders to each mesh object
+        /// </summary>
+        public bool addMeshCollider
+        {
+            get;
+        }
+        
+        /// <summary>
+        /// Adds a mesh renderer to each object, helpful if you want to see what you're looking at
+        /// </summary>
+        public bool addMeshRenderer
+        {
+            get;
+        }
+        /// <summary>
+        /// Repositions the origin of the mesh object to the center of the mesh bounds 
+        /// </summary>
+        public bool recenterTransform
+        {
+            get;
+        }
+        /// <summary>
+        /// Reference the speckle mesh material for the mesh renderer
+        /// </summary>
+        public bool useRenderMaterial
+        {
+            get;
+        }
+        
+        /// <summary>
+        /// Default material for a speckle unity object
+        /// </summary>
+        public UnityEngine.Material defaultMaterial
+        {
+            get;
+        }
+        
+        /// <summary>
+        /// Combines all the meshes in a speckle object into a single instance. Useful for reducing draw calls
+        /// </summary>
+        public bool combineMeshes
+        {
+            get;
+        }
+    }
+
+}
---
+++ Packages/com.speckle.core.component-conveter/Runtime/Interfaces/IWantContextObj.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Speckle.Core.Models;
+
+namespace Speckle.ConnectorUnity.Core.ScriptableConverter
+{
+
+    public interface IWantContextObj
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public List<ApplicationObject> contextObjects
+        {
+            get;
+            set;
+        }
+    }
+
+}
