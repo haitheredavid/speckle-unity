a96ca005b9566fe912f9fb34cc92f49778ca237a
David Morgan <30870056+haitheredavid@users.noreply.github.com>
2023-05-09 18:55:00 -0400

    added temporary speckle mesh data

--- Packages/com.speckle.objects/Runtime/SpeckleMesh.cs
+++ Packages/com.speckle.objects/Runtime/SpeckleMesh.cs
@@ -1,5 +1,23 @@
+using System.Collections.Generic;
+using UnityEngine;
+
 namespace Speckle.ConnectorUnity.Core.ScriptableConverter.Components
 {
-	public class SpeckleMesh : BaseBehaviour
-	{ }
+
+    public class SpeckleMesh : BaseBehaviour
+    {
+
+        public struct Data
+        {
+            public List<Vector2> uvs;
+
+            public List<Color> vertexColors;
+
+            public List<Vector3> vertices;
+
+            public List<List<int>> subMeshes;
+
+        }
+    }
+
 }
